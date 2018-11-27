a618b76ea2dfe40f5ac05de4dec13cf2b943c6b3
Nelio Alves <acenelio@gmail.com>
2018-11-26 22:05:10 -0200

    Initial implementation

---
+++ Course/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Course.Entities.Enums;
+
+namespace Course.Entities {
+    class Order {
+
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString() {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+    }
+}
--- Course/Program.cs
+++ Course/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using Course.Entities;
+using Course.Entities.Enums;
 
-namespace Course
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
         }
     }
 }
